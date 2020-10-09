278d6d13fa28f1b4ed7807ea18788c8da22ec642
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-09 14:43:48 -0300

    adding tokens

---
+++ tokens/js/fonts.js
@@ -0,0 +1,21 @@
+const fonts = {
+    family: {
+        head: 'MarkPro',
+        body: 'Roboto-Regular',
+        condensed: 'Roboto Condensed',
+        monospace: 'Consolas',
+    },
+    weight: {
+        bold: '700',
+        medium: '500',
+        regular: '400',
+        light: '300',
+    },
+    fontSize: {
+        xxxs: 10,
+        xxs: 12,
+        xs: 14,
+        sm: 16,
+        md: 18,
+    },
+}
