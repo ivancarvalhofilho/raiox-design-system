08b81a9b9e4108c819a293ca559cadaddb24d251
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-09 14:44:14 -0300

    adding tokens

--- tokens/js/index.js
+++ tokens/js/index.js
@@ -1,7 +1,11 @@
 import colors from "./colors";
+import fonts from "./fonts";
+import styles from "./styles";
 
 const theme = {
-    colors
+    colors,
+    fonts,
+    styles
 }
 
 export default theme
