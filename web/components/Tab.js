55207b2a6e5622fbf748a043c50eb587518cd669
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-16 12:10:08 -0300

    fixing tabs

--- web/components/Tab.js
+++ web/components/Tab.js
@@ -3,28 +3,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TabContainer = styled.div`
-  padding: 5px;
+  padding: 5px 0px;
   font-family: ${(props) => props.theme.fonts.family.body};
   font-weight: ${(props) =>
     props.active
-        ? props.theme.fonts.weight.medium
-        : props.theme.fonts.weight.regular};
+      ? props.theme.fonts.weight.medium
+      : props.theme.fonts.weight.regular};
   width: ${(props) => props.width}%;
   text-align: center;
   color: ${(props) =>
     props.active
-        ? props.theme.colors.brand.primary.darkest
-        : props.theme.colors.neutral.dark['01']};
+      ? props.theme.colors.brand.primary.darkest
+      : props.theme.colors.neutral.dark['01']};
   font-size: ${(props) => props.theme.fonts.fontSize.xxs};
 `
 const Tab = (props) => (
-    <TabContainer
-        width={props.width}
-        onClick={props.onClick}
-        active={props.active}
-    >
-      {props.label}
-    </TabContainer>
+  <TabContainer
+    width={props.width}
+    onClick={props.onClick}
+    active={props.active}
+  >
+    {props.label}
+  </TabContainer>
 )
 
 export default Tab
