3e27b6e0b6da98b4e0248fa6903bd4da1716e3ec
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-19 12:11:19 -0300

    fixing animation

--- web/components/SideModal.js
+++ web/components/SideModal.js
@@ -44,7 +44,11 @@ const SideModal = (props) => {
 
   return (
     <BackgroundContainer show={props.show ? props.show : show}>
-      <LeftModalContainer ref={ref} show={props.show} width={width}>
+      <LeftModalContainer
+        ref={ref}
+        show={!props.show ? props.show : show}
+        width={width}
+      >
         <LeftModalHeader>
           <Title>{props.title}</Title>
           {props.closable && (
