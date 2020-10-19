9c71d4fa3465f019016df5dd6d96019ceaaaad15
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-19 16:46:37 -0300

    update components

--- web/dist/Icon.js
+++ web/dist/Icon.js
@@ -17,6 +17,12 @@ var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _templateObject() {
   var data = _taggedTemplateLiteral(["\n  cursor: ", ";\n"]);
 
@@ -67,7 +73,7 @@ var Icon = function Icon(props) {
         return _js["default"].colors.brand.primary.darkest;
 
       default:
-        return _js["default"].colors.neutral.dark['02'];
+        return props.appearance || _js["default"].colors.neutral.dark['02'];
     }
   }
 
@@ -82,7 +88,9 @@ var Icon = function Icon(props) {
   return /*#__PURE__*/_react["default"].createElement(IconContainer, {
     onClick: props.onClick
   }, /*#__PURE__*/_react["default"].createElement(_reactSvgInline["default"], {
-    style: props.style,
+    style: _objectSpread(_objectSpread({}, props.style), {}, {
+      display: 'flex'
+    }),
     svg: props.path ? props.path : _js["default"].icons[props.name],
     fill: getIconSvgColor(props),
     width: getIconSvgSize(props)
--- web/dist/StyledComponents.js
+++ web/dist/StyledComponents.js
@@ -3,11 +3,21 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Title = exports.Card = exports.TextRow = void 0;
+exports.Divider = exports.Title = exports.Card = exports.TextRow = void 0;
 
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n  border-left: 1px solid ", ";\n  margin: 1% 0;\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
 
 function _templateObject3() {
   var data = _taggedTemplateLiteral(["\n  font-family: ", ";\n  font-weight: ", ";\n  font-size: ", ";\n  color: ", ";\n"]);
@@ -30,7 +40,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  display: flex;\n  align-items: center;\n  font-family: ", ";\n  font-weight: ", ";\n  font-size: ", ";\n  color: ", ";\n"]);
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  align-items: center;\n  justify-content: ", ";\n  font-family: ", ";\n  font-weight: ", ";\n  font-size: ", ";\n  color: ", ";\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -42,13 +52,15 @@ function _templateObject() {
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
 var TextRow = _styledComponents["default"].div(_templateObject(), function (props) {
+  return props.justifyContent;
+}, function (props) {
   return props.theme.fonts.family.body;
 }, function (props) {
   return props.bold ? props.theme.fonts.weight.medium : props.theme.fonts.weight.regular;
 }, function (props) {
   return props.theme.fonts.fontSize.xs;
 }, function (props) {
-  return props.danger ? props.theme.colors.feedback.danger.dark : props.theme.colors.neutral.dark.base;
+  return props.color ? props.color : props.danger ? props.theme.colors.feedback.danger.dark : props.theme.colors.neutral.dark.base;
 });
 
 exports.TextRow = TextRow;
@@ -70,3 +82,9 @@ var Title = _styledComponents["default"].div(_templateObject3(), function (props
 });
 
 exports.Title = Title;
+
+var Divider = _styledComponents["default"].div(_templateObject4(), function (props) {
+  return props.theme.colors.neutral.dark['03'];
+});
+
+exports.Divider = Divider;
