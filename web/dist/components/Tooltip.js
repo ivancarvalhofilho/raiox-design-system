f2b90aa8b817ef1841661036448e022f312b60a4
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-23 12:35:00 -0300

    fixing tooltip

--- web/dist/components/Tooltip.js
+++ web/dist/components/Tooltip.js
@@ -1,13 +1,11 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 
@@ -19,34 +17,8 @@ var _js = _interopRequireDefault(require("../../../tokens/js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  ", ";\n"]);
-
-  _templateObject2 = function _templateObject2() {
-    return data;
-  };
-
-  return data;
-}
-
 function _templateObject() {
-  var data = _taggedTemplateLiteral([""]);
+  var data = _taggedTemplateLiteral(["\n  ", ";\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -57,42 +29,11 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var TooltipContainer = _styledComponents["default"].div(_templateObject());
-
-var TooltipMessage = _styledComponents["default"].div(_templateObject2(), function (props) {
+var TooltipMessage = _styledComponents["default"].div(_templateObject(), function (props) {
   return (0, _FontUtil["default"])(props.theme, 'body', 'regular', 'xxs', 'neutral.light.base');
 });
 
 var Tooltip = function Tooltip(props) {
-  var _useState = (0, _react.useState)(false),
-      _useState2 = _slicedToArray(_useState, 2),
-      show = _useState2[0],
-      setShow = _useState2[1];
-
-  var _useState3 = (0, _react.useState)(0),
-      _useState4 = _slicedToArray(_useState3, 2),
-      height = _useState4[0],
-      setHeight = _useState4[1];
-
-  var _useState5 = (0, _react.useState)(0),
-      _useState6 = _slicedToArray(_useState5, 2),
-      width = _useState6[0],
-      setWidth = _useState6[1];
-
-  var _useState7 = (0, _react.useState)(0),
-      _useState8 = _slicedToArray(_useState7, 2),
-      widthTooltip = _useState8[0],
-      setWidthTooltip = _useState8[1];
-
-  var container = (0, _react.useRef)();
-  var tooltipRef = (0, _react.useRef)();
-  (0, _react.useEffect)(function () {
-    if (show) {
-      setHeight(container.current.clientHeight);
-      setWidth(tooltipRef.current.clientWidth - container.current.clientWidth);
-      setWidthTooltip(tooltipRef.current.clientWidth);
-    }
-  }, [show]);
   return props.message ? /*#__PURE__*/_react["default"].createElement(_reactTooltipLite["default"], {
     background: _js["default"].colors.neutral.dark.base,
     content: /*#__PURE__*/_react["default"].createElement(TooltipMessage, null, props.message)
--- web/src/components/Tooltip.js
+++ web/src/components/Tooltip.js
@@ -1,10 +1,9 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import fontStyleMaker from '../utils/FontUtil'
 import TooltipLite from 'react-tooltip-lite'
 import theme from '../../../tokens/js'
 
-const TooltipContainer = styled.div``
 const TooltipMessage = styled.div`
   ${(props) =>
     fontStyleMaker(
@@ -15,21 +14,8 @@ const TooltipMessage = styled.div`
       'neutral.light.base',
     )};
 `
-const Tooltip = (props) => {
-  const [show, setShow] = useState(false)
-  const [height, setHeight] = useState(0)
-  const [width, setWidth] = useState(0)
-  const [widthTooltip, setWidthTooltip] = useState(0)
-  const container = useRef()
-  const tooltipRef = useRef()
-  useEffect(() => {
-    if (show) {
-      setHeight(container.current.clientHeight)
-      setWidth(tooltipRef.current.clientWidth - container.current.clientWidth)
-      setWidthTooltip(tooltipRef.current.clientWidth)
-    }
-  }, [show])
-  return props.message ? (
+const Tooltip = (props) =>
+  props.message ? (
     <TooltipLite
       background={theme.colors.neutral.dark.base}
       content={<TooltipMessage>{props.message}</TooltipMessage>}
@@ -39,6 +25,5 @@ const Tooltip = (props) => {
   ) : (
     props.children
   )
-}
 
 export default Tooltip
