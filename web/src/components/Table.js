0f99c4c0285cb5dcecd15441da9b0f5996fe1c27
Gustavo Costa <gustavo.neves@equals.com.br>
2020-10-28 13:49:03 -0300

    fixing table

--- web/src/components/Table.js
+++ web/src/components/Table.js
@@ -135,7 +135,7 @@ const Column = styled.div`
 const Row = styled.div`
   display: flex;
   //width: 100%;
-  overflow: auto;
+  overflow: hidden;
   align-items: center;
   justify-content: ${(props) =>
     props.justify === 'right'
