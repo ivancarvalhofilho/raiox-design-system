967d92b6bc1171149a67ba23dce07dcc493a155f
Gustavo Costa <gustavo.neves@equals.com.br>
2020-11-11 15:41:46 -0300

    fixing theme

---
+++ tokens/theme/colors.js
@@ -0,0 +1,76 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var Colors = {
+  brand: {
+    primary: {
+      darkest: '#008488',
+      dark: '#40B7B9',
+      medium: '#9FDBDC',
+      light: '#F5FBFB'
+    },
+    secondary: {
+      darkest: '#002740',
+      dark: '#003A60',
+      medium: '#6B809D',
+      light: '#F8F9FD'
+    }
+  },
+  neutral: {
+    dark: {
+      base: '#303133',
+      '01': '#595A5C',
+      '02': '#979899',
+      '03': '#E0E0E0'
+    },
+    light: {
+      base: '#FFFFFF',
+      '01': '#FAFAFA',
+      '02': '#F5F5F5',
+      '03': '#EFEFEF'
+    }
+  },
+  feedback: {
+    success: {
+      darkest: '#215924',
+      dark: '#2E7D32',
+      medium: '#97BE98',
+      light: '#F5F8F5'
+    },
+    info: {
+      darkest: '#01588C',
+      dark: '#0277BD',
+      medium: '#80BBDE',
+      light: '#F5F8F5'
+    },
+    danger: {
+      darkest: '#BF2B2B',
+      dark: '#D32F2F',
+      medium: '#E99797',
+      light: '#FDF5F5'
+    },
+    warning: {
+      darkest: '#E5A100',
+      dark: '#FFB300',
+      medium: '#FFD980',
+      light: '#FFFBF2'
+    },
+    attention: {
+      darkest: '#B23F00',
+      dark: '#E65100',
+      medium: '#F28C55',
+      light: '#F2B391'
+    }
+  },
+  support: {
+    social: {
+      whatsapp: '#455A64',
+      facebook: '#1877F2'
+    }
+  }
+};
+var _default = Colors;
+exports["default"] = _default;
---
+++ tokens/theme/fonts.js
@@ -0,0 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+require("./fontsImport.css");
+
+var fonts = {
+  family: {
+    head: 'Mark Pro',
+    body: 'Roboto',
+    condensed: 'Roboto Condensed',
+    monospace: 'Roboto Mono'
+  },
+  weight: {
+    bold: '700',
+    medium: '500',
+    regular: '400',
+    light: '300'
+  },
+  fontSize: {
+    xxxs: '10px',
+    xxs: '12px',
+    xs: '14px',
+    sm: '16px',
+    md: '18px',
+    mdb: '20px',
+    lg: '24px',
+    llg: '28px',
+    xl: '32px'
+  }
+};
+var _default = fonts;
+exports["default"] = _default;
