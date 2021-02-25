44f7f3ab5885f9fe3b611fcdcca9f66558780d5a
ivan.filho <ivancarvalhofilho@github.com>
2021-02-25 10:32:16 -0300

    RX2-4639 - Criado loading

---
+++ stories/Loading.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Loading from "../src/components/Loading";
+
+export default {
+  title: 'Components/Loading',
+  component: Loading,
+}
+
+const Template = args => {
+  return (
+    <div style={{height: '300px', width: '300px'}}>
+      <Loading {...args}/>
+    </div>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {}
