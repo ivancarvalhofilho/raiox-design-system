25b0494e1c47b7f104765fbdc736da98aa0fb1eb
ivan.filho <ivancarvalhofilho@equals.com.br>
2021-09-21 14:55:32 -0300

    RX2-5664 - Corrigido importacao com bug

--- dist/components/InputLabel.js
+++ dist/components/InputLabel.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import {Tokens} from '../tokens'
 import {fontStyleMaker} from '../utils/FontUtil'
 import {Icon} from "./Icon";
+import {camelize} from "../utils/stringUtil";
 
 const SuspendedLabel = styled.div`
 	position: absolute;
@@ -127,12 +128,6 @@ const InputLabel = (props) => {
 
 	const isPasswordType = props.inputType === 'password';
 
-	function camelize(str) {
-		return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-			return index === 0 ? word.toLowerCase() : word.toUpperCase();
-		}).replace(/\s+/g, '').replace(/\W/gm,'');
-	}
-
 	return (
 		<InputLabelContainer
 			disabled={props.disabled}
@@ -182,5 +177,4 @@ InputLabel.propTypes = {
 	errorLabel: PropTypes.string,
 	inputType: PropTypes.string,
 	autofocus: PropTypes.bool,
-	testID: PropTypes.string,
 }
