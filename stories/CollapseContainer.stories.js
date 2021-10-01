2f35461a4a8276226c16f79594634e6ab9a0cc56
ivan.filho <ivancarvalhofilho@equals.com.br>
2021-10-01 15:18:05 -0300

    RX2-5717 - Ajustes finos no collapseContainer

--- stories/CollapseContainer.stories.js
+++ stories/CollapseContainer.stories.js
@@ -3,15 +3,17 @@ import React from 'react'
 import {CollapseContainer} from '../src'
 
 export default {
-  title: 'Components/CollapseContainer',
-  component: CollapseContainer,
+    title: 'Components/CollapseContainer',
+    component: CollapseContainer,
 }
 
-const Template = args => <CollapseContainer {...args} />
+const Template = args => {
+    return (<div style={{maxWidth: 400, height: '400px'}}><CollapseContainer {...args} /><CollapseContainer {...args} /><CollapseContainer {...args} /></div>)
+}
 
 export const Default = Template.bind({})
 Default.args = {
-  title: 'O que significa “recebido” e “a receber” (acima do calendário?)',
-  children:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    title: 'O que significa “recebido” e “a receber” (acima do calendário?)',
+    children:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 }
