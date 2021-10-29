20c382d6332ade0a4f7499c52ba8da5834f74dab
ivan.filho <ivancarvalhofilho@equals.com.br>
2021-10-29 15:14:42 -0300

    Adicionado build

--- dist/components/InputFilter.js
+++ dist/components/InputFilter.js
@@ -8,7 +8,7 @@ import {fontStyleMaker} from "../utils";
 import {InputLabelContainer} from "./InputLabel";
 
 const InputFilterStyled = styled.input`
-  padding: 16px 16px 16px 44px;
+  padding: 8px 8px 8px 44px;
   box-shadow: inset 0 0 0 1px ${Tokens.colors.neutral.dark['03']};
   border: none;
   box-sizing: border-box;
@@ -21,23 +21,12 @@ const InputFilterStyled = styled.input`
     fontWeight: 'regular',
     fontSize: 'xs',
   })};
+  line-height: 24px;
   width: inherit;
 
   &:focus {
     outline: none;
     box-shadow: inset 0 0 0 2px ${Tokens.colors.brand.primary.darkest};
-  }
-
-  ${props => props.isPasswordType && `
-    padding-right: 100px;
-  `}
-  ${(props) =>
-          props.hasError &&
-          `
-		box-shadow: inset 0 0 0 2px ${Tokens.colors.feedback.danger.dark} !important;
-	`}
-  &:-webkit-autofill {
-    -webkit-background-clip: text;
   }
 `
 const SpanError = styled.span`
@@ -80,6 +69,7 @@ const SpanCancel = styled.span`
     fontSize: 'xs',
   })};
   cursor: pointer;
+  word-break: normal;
 `
 const DivInput = styled.div`
   position: relative;
