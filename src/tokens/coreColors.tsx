c4a2233d4180485cb377b04779f63bbaf80d0ee5
ivan.filho <ivancarvalhofilho@equals.com.br>
2021-11-16 11:54:46 -0300

    Alterado cores do DS Core

--- src/tokens/coreColors.tsx
+++ src/tokens/coreColors.tsx
@@ -4,16 +4,16 @@ const coreColors = {
     ...Colors,
     brand: {
         primary: {
-            darkest: '#0C395D',
-            dark: '#0077B3',
+            darkest: '#0077B3',
+            dark: '#0077B3', // A mudar
             medium: '#C7E1EE',
             light: '#EDF8FD'
         },
         secondary: {
-            darkest: '#303133',
-            dark: '#595A5C',
-            medium: '#979899',
-            light: '#E0E0E0'
+            darkest: '#0C395D',
+            dark: '#595A5C', // A mudar
+            medium: '#979899', // A mudar
+            light: '#E0E0E0' // A mudar
         },
         background: {base:'#F8F9FD'},
     },
