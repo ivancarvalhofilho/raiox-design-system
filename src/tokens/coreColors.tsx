78dfc6eb45d48ae917a0ed37035865210714cbab
ivan.filho <ivancarvalhofilho@equals.com.br>
2021-12-08 12:33:22 -0300

    Fixed coreColors

--- src/tokens/coreColors.tsx
+++ src/tokens/coreColors.tsx
@@ -1,7 +1,6 @@
 import Colors from "./colors";
 
 const coreColors = {
-    ...Colors,
     brand: {
         primary: {
             darkest: '#0068AB',
@@ -16,6 +15,59 @@ const coreColors = {
             light: '#F8F9FD'
         },
         background: {base:'#F8F9FD'},
+    },
+
+    neutral: {
+        dark: {
+            base: '#303133',
+            '01': '#595A5C',
+            '02': '#979899',
+            '03': '#E0E0E0'
+        },
+        light: {
+            base: '#FFFFFF',
+            '01': '#FAFAFA',
+            '02': '#F5F5F5',
+            '03': '#EFEFEF'
+        },
+    },
+    feedback: {
+        success: {
+            darkest: '#215924',
+            dark: '#2E7D32',
+            medium: '#97BE98',
+            light: '#F5F8F5'
+        },
+        info: {
+            darkest: '#01588C',
+            dark: '#0277BD',
+            medium: '#80BBDE',
+            light: '#F5F8F5'
+        },
+        danger: {
+            darkest: '#BF2B2B',
+            dark: '#D32F2F',
+            medium: '#E99797',
+            light: '#FDF5F5'
+        },
+        warning: {
+            darkest: '#E5A100',
+            dark: '#FFB300',
+            medium: '#FFD980',
+            light: '#FFFBF2'
+        },
+        attention: {
+            darkest: '#B23F00',
+            dark: '#E65100',
+            medium: '#F28C55',
+            light: '#F2B391'
+        }
+    },
+    support: {
+        social: {
+            whatsapp: '#455A64',
+            facebook: '#1877F2',
+        },
     },
 }
 
