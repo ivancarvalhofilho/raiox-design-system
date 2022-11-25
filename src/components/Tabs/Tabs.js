c16c277e5311aae573aa78cbdd04faf6fa9c74a7
Vítor Azevedo <vitor.azevedo@equals.com.br>
2022-11-25 11:18:08 -0300

    RX2-7388 Refactoring tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,53 +1,79 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {Tab} from './Tab'
+import { Tab } from './Tab'
 import styled from 'styled-components'
-import {Tokens} from '../../tokens'
+import { Tokens } from '../../tokens'
 
 const TabsContainer = styled.div`
   display: flex;
   justify-content: space-around;
-  pointer-events: ${props => props.disabled && 'none'};
+  pointer-events: ${(props) => props.disabled && 'none'};
+  ${(props) =>
+    props.customStyle?.containerPadding
+      ? `padding-top: ${props.customStyle?.containerPadding}px;`
+      : null}
+  ${(props) =>
+    props.customStyle?.containerPadding
+      ? `padding-bottom: ${props.customStyle?.containerPadding}px;`
+      : null}
 `
 const Container = styled.div`
-  opacity: ${props => props.disabled && '0.5'};
-  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
+  opacity: ${(props) => props.disabled && '0.5'};
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+  background-color: ${(props) =>
+    props.customStyle?.containerColor
+      ? props.customStyle?.containerColor
+      : null};
 `
 const Bar = styled.div`
-  width: ${props => props.width}%;
-  height: ${props => props.height || 4}px;
-  border-radius: ${props => props.rounded && '4px 4px 0px 0px'};
-  background-color: ${props =>
-          props.color || Tokens.colors.brand.primary.darkest};
-  transform: translate(${props => props.left}%, 0);
+  width: ${(props) => props.width}%;
+  height: ${(props) => props.height || 4}px;
+  border-radius: ${(props) => props.rounded && '4px 4px 0px 0px'};
+  background-color: ${(props) =>
+    props.customStyle?.barColor
+      ? props.customStyle.barColor
+      : props.color || Tokens.colors.brand.primary.darkest};
+  transform: translate(${(props) => props.left}%, 0);
   transition: transform 0.3s linear;
 `
 
-const Tabs = props => (
-    <Container disabled={props.disabled} style={props.style}>
-        <TabsContainer disabled={props.disabled}>
-            {props.tabs.map((tab, index) => (
-                <Tab
-                    id={`tab${index}`}
-                    key={index}
-                    label={tab}
-                    width={100 / props.tabs.length}
-                    onClick={() => !props.disabled && props.onTabClick(index)}
-                    active={props.tabActive === index}
-                />
-            ))}
-        </TabsContainer>
+const Tabs = (props) => (
+  <Container
+    disabled={props.disabled}
+    style={props.style}
+    customStyle={props.customStyle}
+  >
+    <TabsContainer disabled={props.disabled} customStyle={props.customStyle}>
+      {props.tabs.map((tab, index) => (
+        <Tab
+          id={`tab${index}`}
+          key={index}
+          label={tab}
+          width={100 / props.tabs.length}
+          onClick={() => !props.disabled && props.onTabClick(index)}
+          active={props.tabActive === index}
+          fontStyle={props.customStyle}
+        />
+      ))}
+    </TabsContainer>
 
-        <Bar width={100 / props.tabs.length} left={props.tabActive * 100} rounded/>
-        <Bar width={100} color={Tokens.colors.neutral.dark['03']} height={1}/>
-    </Container>
+    <Bar
+      width={100 / props.tabs.length}
+      left={props.tabActive * 100}
+      rounded
+      customStyle={props.customStyle}
+    />
+    <Bar width={100} color={Tokens.colors.neutral.dark['03']} height={1} />
+  </Container>
 )
 
-export {Tabs}
+export { Tabs }
 Tabs.propTypes = {
-    disabled: PropTypes.bool,
-    onTabClick: PropTypes.func,
-    tabActive: PropTypes.number,
-    tabs: PropTypes.array,
-    identifier: PropTypes.string,
+  disabled: PropTypes.bool,
+  onTabClick: PropTypes.func,
+  tabActive: PropTypes.number,
+  tabs: PropTypes.array,
+  // eslint-disable-next-line react/no-unused-prop-types
+  identifier: PropTypes.string,
+  customStyle: PropTypes.object,
 }
--- stories/Tabs.stories.js
+++ stories/Tabs.stories.js
@@ -1,29 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-import {Tabs, TabsCore} from '../src'
+import { Tabs, TabsCore } from '../src'
 
 export default {
   title: 'Components/Tabs',
   component: Tabs,
 }
 
-const Template = args => {
+const Template = (args) => {
   const [tabActive, setTabActive] = useState(0)
   return <Tabs {...args} tabActive={tabActive} onTabClick={setTabActive} />
 }
 
-const TemplateCore = args => {
+const TemplateCore = (args) => {
   const [tabActive, setTabActive] = useState(0)
   return <TabsCore {...args} tabActive={tabActive} onTabClick={setTabActive} />
 }
 
 export const Default = Template.bind({})
-export const tabsCore = TemplateCore.bind({})
+export const TabCore = TemplateCore.bind({})
 Default.args = {
   tabs: ['Categorias', 'Maquininhas', 'Banco'],
   disabled: false,
+  customStyle: {
+    containerColor: '#00A868',
+    containerPadding: 10,
+    barColor: '#005C41',
+    fontFamily: '',
+    fontSize: 14,
+    activeFontColor: '#FFFFFF',
+    inactiveFontColor: '#FFFFFFB2',
+    activeTabfontWeight: 700,
+    inactiveTabfontWeight: 600,
+  },
 }
-tabsCore.args = {
+TabCore.args = {
   tabs: ['Categorias', 'Maquininhas', 'Banco'],
   disabled: false,
 }
