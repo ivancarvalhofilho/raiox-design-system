388aa2f5c659e54368f71159f1f3184daedd11f7
Vítor Azevedo <vitor.azevedo@equals.com.br>
2022-12-06 16:04:13 -0300

    Adding prop into List component

--- dist/components/ListHeader.js
+++ dist/components/ListHeader.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
-import {Tokens} from "../tokens";
+import { Tokens } from '../tokens'
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -21,7 +21,9 @@ const ListHeader = props => (
     {props.data.map((key, index) => (
       <Column
         key={index}
-        width={100 / props.data.length}
+        width={
+          props.nowRapRow && index === 0 ? 100 / 2 : 100 / props.data.length
+        }
         align={props.align[index]}
       >
         {key}
@@ -34,4 +36,5 @@ export { ListHeader }
 ListHeader.propTypes = {
   align: PropTypes.array,
   data: PropTypes.array,
+  nowRapRow: PropTypes.any,
 }
